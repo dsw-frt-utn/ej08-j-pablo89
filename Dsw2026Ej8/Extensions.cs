75a05c9bb564160148c5126ccf78af13def22fc7
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2026-05-04 23:55:36 -0300

    Ejercicio 06 Finalizado

---
+++ Dsw2026Ej8/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public static class Extensions
+    {
+        public static string ToProductCode(this string code)
+        {
+            if (code == null)
+            {
+                code = "SIN-CODIGO";
+            }
+            code = code.Trim();
+            code = code.ToUpper();
+            code = code.Replace(" ", "-");
+            return code;
+                
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -6,28 +6,35 @@ namespace Dsw2026Ej8
     {
         static void Main(string[] args)
         {
-            // OPRUEBA EJERCICIO 1.
-            //ProductHelper ph = new ProductHelper();
-            //Console.WriteLine(ph.ObtenerEtiquetaProducto(1, "Coca Zero 2.5", 4500));
+            //OPRUEBA EJERCICIO 1.
+            ProductHelper ph = new ProductHelper();
+            Console.WriteLine(ph.ObtenerEtiquetaProducto(1, "Coca Zero 2.5", 4500));
 
-            // PRUEBA EJERCICIO 2.
-            //var problema2 = new Problema2();
-            //Console.WriteLine(problema2.CrearResumenVenta(1, "Teclado mecánico RGB", 1, 100000));
+            //PRUEBA EJERCICIO 2.
+            var problema2 = new Problema2();
+            Console.WriteLine(problema2.CrearResumenVenta(1, "Teclado mecánico RGB", 1, 100000));
 
-            // PRUEBA EJERCICIo 3.
-            //var problema3 = new Problema3();
-            //Console.WriteLine(problema3.CompararCopias(1, new Product { Description = "Primera Descripcion" }));
+            //PRUEBA EJERCICIo 3.
+            var problema3 = new Problema3();
+            Console.WriteLine(problema3.CompararCopias(1, new Product { Description = "Primera Descripcion" }));
 
-            // PRUEBA EJERCICIO 4.
-            //var problema4 = new Problema4();
-            //Console.WriteLine(problema4.CalcularPromedio(null, 8, 9));
+            //PRUEBA EJERCICIO 4.
+            var problema4 = new Problema4();
+            Console.WriteLine(problema4.CalcularPromedio(null, 8, 9));
 
-            // PRUEBA EJERCICIO 5.
+            //PRUEBA EJERCICIO 5.
             var problema5 = new Problema5();
             Sale s = new WholesaleSale { monto = 1000 };
             Sale s2 = new RetailSale { monto = 1000 };
             Console.WriteLine(problema5.ObtenerImporteFinal(s));
             Console.WriteLine(problema5.ObtenerImporteFinal(s2));
+
+            // PRUEBA EJERCICIO 6.
+            var p6 = new Problema6();
+            Console.WriteLine(p6.NormalizarCodigoProducto(" ab 123 x "));
+
+
+
 
 
 
