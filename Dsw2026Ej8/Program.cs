7f4c930288c95c7d9b59d3acc68467455d7dd56c
Juan Pablo Miranda <jpablomiranda.21@gmail.com>
2026-05-04 22:45:08 -0300

    Ejercicio 01 Finalizado

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProductHelper ph = new ProductHelper();
+            Console.WriteLine(ph.ObtenerEtiquetaProducto(1, "Coca Zero 2.5", 4500));
         }
     }
 }
